ad68e2e2498abafffa9641226832a9418405ae1a
zyf <1048271711@qq.com>
2019-08-21 15:14:06 +0800

    first commit

---
+++ app/src/main/java/com/izhikang/learnkotlin/Sample.kt
@@ -0,0 +1,8 @@
+package com.izhikang.learnkotlin
+
+/**
+ * Created on 2019-08-21
+ * @author zyf
+ */
+class Sample {
+}
---
+++ app/src/main/java/com/izhikang/learnkotlin/Test.kt
@@ -0,0 +1,16 @@
+package com.izhikang.learnkotlin
+
+
+const val Test = "test"
+
+fun testTopLevel(){
+
+}
+
+fun Int.toPx(): Int {
+    return 0
+}
+
+fun dp2px(){
+
+}
