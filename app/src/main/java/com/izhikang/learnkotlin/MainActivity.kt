adfee57e8d012249ec3fecfb5e474fd2d71e1700
zyf <1048271711@qq.com>
2019-08-21 15:15:22 +0800

    fix init

--- app/src/main/java/com/izhikang/learnkotlin/MainActivity.kt
+++ app/src/main/java/com/izhikang/learnkotlin/MainActivity.kt
@@ -1,22 +1,12 @@
 package com.izhikang.learnkotlin
 
 import android.os.Bundle
-import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tvName.text = Test
-        testTopLevel()
-        println()
-        val listener = object :ViewPager.SimpleOnPageChangeListener(){
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-
-            }
-        }
     }
 }
--- app/src/main/java/com/izhikang/learnkotlin/Sample.kt
+++
@@ -1,8 +0,0 @@
-package com.izhikang.learnkotlin
-
-/**
- * Created on 2019-08-21
- * @author zyf
- */
-class Sample {
-}
